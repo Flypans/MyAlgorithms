f1e156c4e4d9671cb59050c93cc8138612c6acbb
Flypans <sbkwon011@outlook.com>
2024-08-19 10:55:38 +0100

    Add NuGet package files for test

---
+++ NuGetPackageTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetPackageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 30, 50, 9, 8, 2, 3 };
+            int[] results = DulAlgorithm.Algorithm.SelectionSort(numbers);
+
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
