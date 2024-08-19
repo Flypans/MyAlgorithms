44b3a14de200f5d7ffd4375d5159ee3302c32c4f
Flypans <sbkwon011@outlook.com>
2024-08-19 10:13:31 +0100

    Add test for MSTest

--- MyAlgorithms.tests/AlgorithmClassTest.cs
+++ MyAlgorithms.tests/AlgorithmClassTest.cs
@@ -5,12 +5,31 @@ using System.Text;
 
 namespace MyAlgorithms.tests
 {
-    [TestClass]
+    [TestClass] 
     public class AlgorithmClassTest
     {
+/*
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            Assert.AreEqual(10, 10);
+            Assert.AreEqual(10, 100);
+
+        }
+*/
         [TestMethod]
         public void TestSelectionSortAscendingOrder()
         {
+            //Step 1) Arrange
+            int[] arr = { 100, 50, 10 };
+
+            //Step 2) Act
+            int[] results = MyAlgorithms.SelectionSort(arr);
+
+            //Step 3) Assert
+            Assert.AreEqual(10, results[0]);// True
+            Assert.AreEqual(100, results[arr.Length - 1]);// True
+
 
         }
     }
