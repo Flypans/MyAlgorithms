5fc33c58b6a2079db65cb3bea03a92ba3929b40b
Flypans <sbkwon011@outlook.com>
2024-08-19 09:26:03 +0100

    Create initial test class with MStext

---
+++ MyAlgorithms.tests/AlgorithmClassTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlgorithms.tests
+{
+    [TestClass]
+    public class AlgorithmClassTest
+    {
+        [TestMethod]
+        public void TestSelectionSortAscendingOrder()
+        {
+
+        }
+    }
+}
